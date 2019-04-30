f89568a20ae1f6fbf5b8a4d3c129a710ce5fe25e
Jordy Langen <jordylangen@gmail.com>
2019-04-30 20:15:48 +0200

    Changed the contract resolver + user domain model again. Now using a public default ctor and private setters only

---
+++ backend/Kenpotatakai.Core/Infrastructure/CamelCaseIncludingPrivateSetterPropertyNamesContractResolver.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Kenpotatakai.Core.Infrastructure
+{
+    public class CamelCaseIncludingPrivateSetterPropertyNamesContractResolver : CamelCasePropertyNamesContractResolver
+    {
+        protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
+        {
+            var property = base.CreateProperty(member, memberSerialization);
+
+            if (property.Writable)
+            {
+                return property;
+            }
+
+            var propertyInfo = member as PropertyInfo;
+            property.Writable = propertyInfo?.SetMethod != null;
+
+            return property;
+        }
+    }
+}
--- backend/Kenpotatakai.Core/Users/User.cs
+++ backend/Kenpotatakai.Core/Users/User.cs
@@ -19,31 +19,9 @@ namespace Kenpotatakai.Core.Users
         public string DisplayName { get; private set; }
         public string AvatarUrl { get; private set; }
 
-        [JsonConstructor]
-        public User(
-            string id,
-            string platformId,
-            string providerId,
-            string providerName,
-            string emailAddress,
-            string displayName,
-            string avatarUrl)
+        public User()
         {
-            Guard.AgainstNonEmpty(id, nameof(Id));
-            Guard.AgainstNonEmpty(platformId, nameof(PlatformId));
-            Guard.AgainstNonEmpty(providerId, nameof(ProviderId));
-            Guard.AgainstNonEmpty(providerName, nameof(ProviderName));
-            Guard.AgainstNonEmpty(displayName, nameof(DisplayName));
-            Guard.AgainstNonEmpty(avatarUrl, nameof(AvatarUrl));
-            Guard.AgainstInvalidEmailAddress(emailAddress);
-
-            Id = id;
-            PlatformId = platformId;
-            ProviderId = providerId;
-            ProviderName = providerName;
-            EmailAddress = emailAddress;
-            DisplayName = displayName;
-            AvatarUrl = avatarUrl;
+            // for serialization purposes only
         }
 
         public User(
