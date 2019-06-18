04513daf0be0720c09c226dd7f76611c8862657c
Jordy Langen <jordylangen@gmail.com>
2019-06-18 14:57:25 +0200

    Revamped some of the styling

--- app/lib/signUp/sign_up_screen.dart
+++ app/lib/signUp/sign_up_screen.dart
@@ -8,6 +8,8 @@ import 'package:kenpotatakai/redux/app_state.dart';
 import 'package:kenpotatakai/signUp/sign_up_state.dart';
 import 'package:kenpotatakai/signUp/sign_up_view_model.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:kenpotatakai/widgets/app_button.dart';
+import 'package:kenpotatakai/widgets/main_container.dart';
 
 class SignUpScreen extends StatefulWidget {
   @override
@@ -27,8 +29,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
 
   Widget _build(BuildContext context, SignUpViewModel viewModel) {
     var body = _buildProviderOverview(viewModel);
-
-    return Scaffold(backgroundColor: AppColors.primaryColor, body: body);
+    return Scaffold(body: MainContainer.primary(child: body));
   }
 
   Widget _buildProviderOverview(SignUpViewModel viewModel) {
@@ -37,12 +38,12 @@ class _SignUpScreenState extends State<SignUpScreen> {
     return Center(
       child: ListView(shrinkWrap: true, padding: EdgeInsets.only(left: 24.0, right: 24.0), children: [
         Text('Welcome to Kenpotatakai!',
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold), textAlign: TextAlign.center),
+            style: TextStyle(fontSize: 21, fontWeight: FontWeight.bold), textAlign: TextAlign.center),
         SizedBox(height: 16.0),
         Text('Use any of the providers listed below to create an account so you can use this app.',
-            style: TextStyle(fontSize: 12), textAlign: TextAlign.center),
+            style: TextStyle(fontSize: 14), textAlign: TextAlign.center),
         SizedBox(height: 32.0),
-        twitterRow
+        twitterRow,
       ]),
     );
   }
@@ -51,30 +52,14 @@ class _SignUpScreenState extends State<SignUpScreen> {
     var providerName = capitalize(provider.toString().split('.').last);
     final text = 'Sign up with ' + providerName;
 
-    return ClipRRect(
-        borderRadius: BorderRadius.circular(32.0),
-        child: FlatButton(
-            padding: EdgeInsets.all(0),
-            child: Container(
-                padding: EdgeInsets.all(4),
-                color: AppColors.accentColor,
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Padding(
-                          padding: EdgeInsets.only(right: 16.0, top: 8.0, bottom: 8.0),
-                          child: Hero(
-                              tag: providerName.toLowerCase(),
-                              child: Icon(
-                                icon,
-                                color: Colors.white,
-                              ))),
-                      Text(text, style: TextStyle(color: Colors.white))
-                    ])),
-            onPressed: () {
-              viewModel.signUpAt(provider);
-              Navigator.pushNamed(context, Routes.SignUpAtProvider);
-            }));
+    return AppButton(
+      text: text,
+      icon: FontAwesomeIcons.twitter,
+      tag: providerName,
+      onPressed: () {
+        viewModel.signUpAt(provider);
+        Navigator.pushNamed(context, Routes.SignUpAtProvider);
+      },
+    );
   }
 }
